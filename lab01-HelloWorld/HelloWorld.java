b6450c71d07df9e1829aa6fea0727a8c93927c36
Robert Storer <rws216@lehigh.edu>
2015-01-23 19:56:09 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////
+//Robert Storer
+//CSE2 Lab 1
+
+//Goal - Print Hello World Message
+
+//1st: Define Class
+//Class is cotnainer that holds program (can call it anything)
+public class HelloWorld{
+
+    //add main method 
+    //main method is where computer starts reading program    
+    public static void main(String[] args) {
+        
+        //Now I can tell the computer what text I want it to print
+        //the System.out.println("--------------"); command tells computer to print what is written within the parentheses
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        
+        //This code should work. Compile program and run the code. Hopefully no bugs.
+        //command javac is used to compile program
+        //command java is used to run program
+        
+
+}
+
+
+}
