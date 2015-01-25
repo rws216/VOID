9783f3f6947ec712a85d468458e45d78e191c754
Robert Storer <rws216@lehigh.edu>
2015-01-25 20:33:24 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+/////////////////////////////////////////////////////////////////////////////
+//Robert Storer
+//January 23, 2015
+//CSE002 - HW1
+//WelcomeClass Java Program
+
+//Goal - To have the computer print a welcome to the class message
+//Use basic skills learned from lab 1 to perform a similar task
+
+//Again, the first step is to define a class
+public class WelcomeClass {
+    
+    //Now, we need to add the main method
+    public static void main(String[] args) {
+        
+        //The computer will begin reading code from here. 
+        //I will use the print command to print my welcome message
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-R--W--S--2--1--6->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("The first week of the semester went very well.");
+        System.out.println("I'm excited to see where 2015 will take me.");
+        
+        //Don't forget to close any open brackets
+    }
+    
+}
